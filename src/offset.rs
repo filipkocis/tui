18ff05234dea60600e5a7b23ca217cf5778b39f0
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-05-07 23:05:21 +0200

    use i16 in offset

--- src/offset.rs
+++ src/offset.rs
@@ -1,14 +1,12 @@
-use std::ops::Add;
-
 #[derive(Debug, Clone, Copy)]
 pub enum Offset {
-    Absolute(u16, u16),
-    Translate(u16, u16),
+    Absolute(i16, i16),
+    Translate(i16, i16),
 }
 
 impl Offset {
     #[inline(always)]
-    pub fn x(&self) -> u16 {
+    pub fn x(&self) -> i16 {
         match self {
             Self::Absolute(x, _) => *x,
             Self::Translate(x, _) => *x,
@@ -16,7 +14,7 @@ impl Offset {
     }
 
     #[inline(always)]
-    pub fn y(&self) -> u16 {
+    pub fn y(&self) -> i16 {
         match self {
             Self::Absolute(_, y) => *y,
             Self::Translate(_, y) => *y,
@@ -34,19 +32,25 @@ impl Offset {
     }
 
     #[inline(always)]
-    pub fn tuple(self) -> (u16, u16) {
+    pub fn tuple(self) -> (i16, i16) {
         match self {
             Self::Absolute(x, y) => (x, y),
             Self::Translate(x, y) => (x, y),
         }
     }
 
+    #[inline(always)]
     pub fn add(self, child: Self) -> Self {
         match (self, child) {
             (Self::Translate(x1, y1), Self::Translate(x2, y2)) => Self::Translate(x1 + x2, y1 + y2),
             (Self::Absolute(x1, y1), Self::Translate(x2, y2)) => Self::Translate(x1 + x2, y1 + y2),
             (_, Self::Absolute(..)) => child,
         }
+    }
+
+    #[inline(always)]
+    pub fn add_tuple(self, tuple: (i16, i16)) -> Self {
+        self.add(Self::Translate(tuple.0, tuple.1))
     }
 }
 
