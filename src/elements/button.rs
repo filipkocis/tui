7c7f09d6ec812bbab483d676f7913b2cc9b7017a
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-05-11 23:05:59 +0200

    chore: rustfmt

--- src/elements/button.rs
+++ src/elements/button.rs
@@ -13,7 +13,9 @@ pub struct MouseClickEvent {
 pub type MouseClickHandler = Box<dyn FnMut(MouseClickEvent, &mut Node) -> bool>;
 
 /// Generates an event handler for a mouse click event.
-pub fn on_click_handler(mut on_click: impl FnMut(MouseClickEvent, &mut Node) -> bool + 'static) -> impl IntoEventHandler {
+pub fn on_click_handler(
+    mut on_click: impl FnMut(MouseClickEvent, &mut Node) -> bool + 'static,
+) -> impl IntoEventHandler {
     move |event: &Event, node: &mut Node| {
         if let Event::Mouse(mouse_event) = event {
             let button = match mouse_event.kind {
@@ -25,14 +27,14 @@ pub fn on_click_handler(mut on_click: impl FnMut(MouseClickEvent, &mut Node) -> 
             let y = mouse_event.row as i16;
 
             if node.hit_test(x, y) {
-                let click_event = MouseClickEvent { 
+                let click_event = MouseClickEvent {
                     button,
                     relative: node.relative_position(x, y),
                     modifiers: mouse_event.modifiers,
                 };
 
-                return on_click(click_event, node)
-            } 
+                return on_click(click_event, node);
+            }
         }
         false
     }
@@ -46,7 +48,7 @@ impl Button {
 
         node.content = label;
         node.style.size = Size::from_cells(len, 1);
-        node.style.grow = true; 
+        // node.style.grow = true;
 
         // node.add_handler(
         //     on_click_handler(|event, node| {
