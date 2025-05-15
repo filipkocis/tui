672972de90b07c000fb0fff7fbce59088f75f434
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-05-15 19:49:20 +0200

    feat: implement dispatching mouse event for all phases

--- src/lib.rs
+++ src/lib.rs
@@ -94,22 +94,25 @@ impl App {
         self.resize(self.viewport.screen.0, self.viewport.screen.1)?;
 
         loop {
+            let mut resize = None;
             let mut render = false;
 
             while event::poll(Duration::from_millis(0))? {
+                let event = event::read()?;
+
                 use event::*;
-                match event::read()? {
+                match event {
                     Event::Key(event) => match event.code {
                         KeyCode::Esc => {
                             return Ok(());
                         }
                         event => println!("{event:?}"),
                     },
-                    Event::Mouse(event) => {
-                        println!("{event:?}");
+                    Event::Mouse(_event) => {
+                        self.dispatch_mouse_event(&event);
                     }
                     Event::Resize(width, height) => {
-                        self.resize(width, height)?;
+                        resize = Some((width, height));
                         println!("Resize {width}x{height}")
                     }
                     event => println!("{event:?}"),
@@ -118,8 +121,71 @@ impl App {
                 render = true;
             }
 
+            if let Some((width, height)) = resize.take() {
+                self.resize(width, height)?;
+            }
+
             if render {
                 self.render()?;
+            }
+        }
+    }
+
+    /// Returns the path from the target node `id` to the root node.
+    /// TODO: temporary solution, remove in the future
+    pub fn get_path_to(&self, id: NodeId) -> Option<Vec<Rc<RefCell<Node>>>> {
+        let mut path = Vec::new();
+
+        if self.root.borrow().build_path_to_node(id, &mut path) {
+            path.push(self.root.0.clone());
+            Some(path)
+        } else {
+            None
+        }
+    }
+
+    /// Dispatches a mouse event to the target node based on the hitmap.
+    pub fn dispatch_mouse_event(&mut self, event: &Event) {
+        let (column, row) = match event {
+            Event::Mouse(event) => (event.column, event.row),
+            _ => return,
+        };
+
+        let Some(target) = self.hitmap.get(column, row) else {
+            return;
+        };
+
+        let Some(path) = self.get_path_to(target) else {
+            return;
+        };
+
+        let Some(target) = path.first() else {
+            return;
+        };
+
+        // Capture phase
+        for node in path.iter().skip(1).rev() {
+            let mut node = node.borrow_mut();
+            if node.handle_event(event, true) {
+                return;
+            }
+        }
+
+        // Target phase
+        {
+            let mut node = target.borrow_mut();
+            let capture = node.handle_event(event, true);
+            let bubble = node.handle_event(event, false);
+            if capture || bubble {
+                return;
+            }
+        }
+
+        // Bubble phase
+        for node in path.iter().skip(1) {
+            let mut node = node.borrow_mut();
+            if node.handle_event(event, false) {
+                return;
             }
         }
     }
