b1b4a33890ce0e04c855aebd945b0f8964a281ca
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-06-03 19:16:41 +0200

    feat: impl VisualGrapheme

--- src/text/visual.rs
+++ src/text/visual.rs
@@ -10,6 +10,18 @@ pub struct StyleSpan {
     pub length: usize,
 }
 
+#[derive(Debug, Clone)]
+pub struct VisualGrapheme {
+    /// The grapheme as a string, can be a normal string but needs to have a valid width
+    str: String,
+    /// The visual width of the grapheme in columns
+    width: usize,
+    /// This grapheme's grapheme index in the original line. Used in conjunction with
+    /// `line_index` from [`VisualLine`].
+    /// It will be `None` if the grapheme is not a part of the original line.
+    grapheme_index: Option<usize>,
+}
+
 impl StyleSpan {
     /// Creates a new style span
     pub fn new(code: Code, line: usize, character: usize, length: usize) -> Self {
@@ -18,6 +30,17 @@ impl StyleSpan {
             line,
             character,
             length,
+        }
+    }
+}
+
+impl VisualGrapheme {
+    #[inline]
+    pub fn new(str: String, width: usize, index: Option<usize>) -> Self {
+        Self {
+            str,
+            width,
+            grapheme_index: index,
         }
     }
 }
