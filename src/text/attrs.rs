7506e75d711be359beb67e43abcf4eed34d631c3
Filip Kocis <78610976+filipkocis@users.noreply.github.com>
2025-07-16 22:07:29 +0200

    refactor Attrs to use set unset methods

--- src/text/attrs.rs
+++ src/text/attrs.rs
@@ -43,74 +43,90 @@ impl Attrs {
             .collect()
     }
 
-    /// Applies the given attrivute to the current attributes and returns a new `Attrs` bitfield.
+    /// Sets the bit at position `n` in the bitfield.
+    #[inline(always)]
+    pub fn set(self, n: u16) -> Self {
+        Self(self.0 | (1 << n))
+    }
+
+    /// Unsets the bit at position `n` in the bitfield.
+    #[inline(always)]
+    pub fn unset(self, n: u16) -> Self {
+        Self(self.0 & !(1 << n))
+    }
+
+    /// Unsets all underline attributes.
+    pub fn unset_underline(self) -> Attrs {
+        self.unset(3) // underline
+            .unset(4) // double underline
+            .unset(5) // undercurled
+            .unset(6) // underdotted
+            .unset(7) // underdashed
+    }
+
+    /// Applies the given attribute to the current attributes and returns a new `Attrs` bitfield.
     pub fn apply(self, attr: Attribute) -> Self {
-        /// Sets the bit at position `n` in `num`.
-        #[inline(always)]
-        fn set(num: u16, n: u16) -> u16 {
-            num | (1 << n)
-        }
+        match attr {
+            Attribute::Reset => Self::default(),
 
-        /// Unsets the bit at position `n` in `num`.
-        #[inline(always)]
-        fn unset(num: u16, n: u16) -> u16 {
-            num & !(1 << n)
-        }
+            Attribute::Bold => self.set(0),
+            Attribute::NoBold => self.unset(0),
 
-        /// Unsets all underline bits.
-        fn unset_underline(num: u16) -> u16 {
-            let ul = unset(num, 3);
-            let du = unset(ul, 4);
-            let uc = unset(du, 5);
-            let ud = unset(uc, 6);
-            unset(ud, 7)
-        }
+            Attribute::Italic => self.set(1),
+            Attribute::NoItalic => self.unset(1),
 
-        let n = self.0;
-        let new = match attr {
-            Attribute::Reset => 0,
+            Attribute::Dim => self.set(2),
+            Attribute::NormalIntensity => self.unset(0).unset(1).unset(2),
 
-            Attribute::Bold => set(n, 0),
-            Attribute::NoBold => unset(n, 0),
+            Attribute::Underlined => self.unset_underline().set(3),
+            Attribute::DoubleUnderlined => self.unset_underline().set(4),
+            Attribute::Undercurled => self.unset_underline().set(5),
+            Attribute::Underdotted => self.unset_underline().set(6),
+            Attribute::Underdashed => self.unset_underline().set(7),
 
-            Attribute::Italic => set(n, 1),
-            Attribute::NoItalic => unset(n, 1),
+            Attribute::SlowBlink => self.set(8),
+            Attribute::RapidBlink => self.set(9),
+            Attribute::NoBlink => self.unset(8).unset(9),
 
-            Attribute::Dim => set(n, 2),
-            Attribute::NormalIntensity => unset(unset(unset(n, 0), 1), 2),
+            Attribute::Reverse => self.set(10),
+            Attribute::NoReverse => self.unset(10),
 
-            Attribute::Underlined => set(unset_underline(n), 3),
-            Attribute::DoubleUnderlined => set(unset_underline(n), 4),
-            Attribute::Undercurled => set(unset_underline(n), 5),
-            Attribute::Underdotted => set(unset_underline(n), 6),
-            Attribute::Underdashed => set(unset_underline(n), 7),
-            Attribute::NoUnderline => unset_underline(n),
+            Attribute::Hidden => self.set(11),
+            Attribute::NoHidden => self.unset(11),
 
-            Attribute::SlowBlink => set(n, 8),
-            Attribute::RapidBlink => set(n, 9),
-            Attribute::NoBlink => 25,
+            Attribute::CrossedOut => self.set(12),
+            Attribute::NotCrossedOut => self.unset(12),
 
-            Attribute::Reverse => set(n, 10),
-            Attribute::NoReverse => unset(n, 10),
-
-            Attribute::Hidden => set(n, 11),
-            Attribute::NoHidden => unset(n, 11),
-
-            Attribute::CrossedOut => set(n, 12),
-            Attribute::NotCrossedOut => unset(n, 12),
-
-            Attribute::OverLined => set(n, 13),
-            Attribute::NotOverLined => unset(n, 13),
+            Attribute::OverLined => self.set(13),
+            Attribute::NotOverLined => self.unset(13),
 
             // Ignored
             Attribute::Fraktur
             | Attribute::Framed
             | Attribute::Encircled
-            | Attribute::NotFramedOrEncircled => n,
+            | Attribute::NotFramedOrEncircled => self,
             // Any new unsupported attrs
-            _ => n,
-        };
+            _ => self,
+        }
+    }
+}
 
-        Self(new)
+impl Iterator for Attrs {
+    type Item = Attribute;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.is_empty() {
+            return None;
+        }
+
+        for i in 0..Self::ATTRS.len() {
+            if self.0 & (1 << i) != 0 {
+                let attr = Self::ATTRS[i];
+                self.0 &= !(1 << i); // Unset the bit
+                return Some(attr);
+            }
+        }
+
+        None
     }
 }
